8b5797d33ed2fa0641ff3318aa51c72a16b699e6
vladmikhbond <vladmikhbond@gmale.com>
2026-02-15 08:20:11 +0000

    .

--- app/static/js/ticket_show.js
+++ app/static/js/ticket_show.js
@@ -1,38 +1,46 @@
-// const codeArea = document.getElementById("codeArea");
+
 const timeRange = document.getElementById("timeRange");
 const checkDiv = document.getElementById("checkDiv");
 const stepTime = document.getElementById("stepTime");
-
+const codeArea = document.getElementById('codeArea');
+const canvas1 = document.getElementById('canvas1');
 
 // константи LINK_SEP і CHECK_SEP у файлі trace_const.js
 // константа track_in_base64 на веб сторінці
 
+// розділяє трек на код и відповідь перевірки
 const bytes = Uint8Array.from(atob(track_in_base64), c => c.charCodeAt(0));
 const track =  new TextDecoder("utf-8").decode(bytes);
-
 const screens = unfold(track);
 const [codes, checks] = separate(screens);
 codes.push(codeArea.value);
 checks.push(checkDiv.innerHTML);
-
+// готує слайдер
 timeRange.max = codes.length - 1;
 timeRange.value = timeRange.max;
 timeRange.focus();
 
-// const isoString = start_time.trim().replace(" ", "T");
-// const date = new Date(isoString);
 
-
+// Anime
 timeRange.addEventListener("change", (e) => {
     let i = timeRange.value;
+    // code
     codeArea.value = codes[i];
     checkDiv.innerHTML = checks[i];
     codeArea.className = checks[i].indexOf('OK') != -1 ? "ok" : "wrong";
-    
+    // time
     let s = i * 3 % 60, m = i * 3 / 60 | 0;
     stepTime.innerHTML = `+${m}' ${s}"`;
-
 })
+
+// Синхронізує розміри textarea і canvas
+new ResizeObserver(syncSize).observe(codeArea);
+syncSize();
+window.addEventListener('resize', syncSize);
+
+// малює діаграму на канвасі
+  let ctx = canvas1.getContext("2d");
+  ctx.fillRect(10, 10, 100, 100);
 
 // Розгортає трек у масив знімків.
 //
@@ -80,5 +88,18 @@ function separate(shots) {
     }
     return [codes, checks];
 }
+
+
   
+  // Синхронізує розміри textarea і canvas
+  //  
+  function syncSize() {
+    const rect = codeArea.getBoundingClientRect();
+    canvas1.width = codeArea.offsetWidth;
+    canvas1.height = codeArea.offsetHeight;
+    // CSS-розмір
+    canvas1.style.width = codeArea.offsetWidth + 'px';
+    canvas1.style.height = codeArea.offsetHeight + 'px';
+  }
+
 
