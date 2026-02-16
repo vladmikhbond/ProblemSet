588fc0e11f7a0a12247e7eba4f00376146f0903d
vladmikhbond <vladmikhbond@gmale.com>
2026-02-16 14:45:20 +0000

    .

--- app/static/js/problemset_edit.js
+++ app/static/js/problemset_edit.js
@@ -1,3 +1,5 @@
+// При зміні чекбоксу будь-якого задачі оновлює список задач в елементі problem_ids
+//
 function reset_problem_ids(e) 
 {   
     const input = e.target;
@@ -17,3 +19,27 @@ function reset_problem_ids(e)
 }
 
 
+const openTime = document.getElementById("openTime");
+const buttonNow = document.getElementById("buttonNow");
+
+// Встановити в елемент openTime поточний київский час (формат YYYY-MM-DD HH:MM)
+//
+buttonNow.addEventListener("click", (e) => {
+    e.preventDefault();
+    const now = new Date();
+
+    const parts = new Intl.DateTimeFormat("en-GB", {
+        timeZone: "Europe/Kyiv",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+    }).formatToParts(now).reduce((acc, p) => {
+        if (p.type !== "literal") acc[p.type] = p.value;
+        return acc;
+    }, {});
+    const value = `${parts.year}-${parts.month}-${parts.day}T${parts.hour}:${parts.minute}`;
+    openTime.value = value;
+})
